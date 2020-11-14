3a7150dead7f9089b65f15eadb247c7d2542a349
Grace-coder311 <workman.1285@osu.edu>
2020-11-14 03:24:04 -0500

    aklfjasdopigjapoihjaeophjfdayopijtopiawgjhear[oighjo[ehjaeoirjhaer[oiytjreaoyes

--- Scripts/tasks_access.js
+++ Scripts/tasks_access.js
@@ -1,11 +1,11 @@
-var tasks = document.getElementById("tasks");
+let tasks = document.getElementById("tasks");
+//let time = new Date();
 
-// need to somehow extract the time element of a task so that a notification can be pumped out
 
 var notifyMsg = "Frick on a stick! You don't have much time";
 time = 0; // this would be in minutes or hours or days????
 
 if(time < 1){ // less than one day
     // notification will appear
-    print(notifyMsg);
+    //print(notifyMsg);
 }
