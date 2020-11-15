0cbe9d7dc1d73871bf10e90dadbdc424d2e2dd8f
Grace-coder311 <workman.1285@osu.edu>
2020-11-14 19:37:42 -0500

    polish checkbox toggle for tasks

---
+++ Scripts/taskCheckbox.js
@@ -0,0 +1,11 @@
+function ShowTasks() {
+    var checkBox = document.getElementById("myCheck");
+    var displaytodo = document.getElementById("ToDo");
+
+  
+    if (checkBox.checked == true) {
+      displaytodo.style.display = "block";
+    } else {
+      displaytodo.style.display = "none";
+    }
+  }
